1b32263228ea806254de776035b064c7a7f621f1
Shubhodeep <banerjeeshubho98@gmail.com>
2023-11-25 19:53:11 +0530

    File-collocation

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
         <div>
-            <h2>404 Not found.</h2>
+            <h2></h2>
             <p>Look for something better</p>
         </div>
     );

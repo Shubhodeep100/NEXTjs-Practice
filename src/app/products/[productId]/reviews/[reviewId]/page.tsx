db9b85bfd0c22f8d211239002dfa0bcac6143241
Shubhodeep <banerjeeshubho98@gmail.com>
2023-11-25 18:16:27 +0530

    Not-found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,12 @@
+import { notFound } from "next/navigation"
+
 export default function Reviews({ params }: {
     params: { productId: string; reviewId: string; }
 }) {
-    return <h1>Reviews of {params.reviewId} for product {params.productId}</h1>
+    if (parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
+    return (
+        <h1>Reviews of {params.reviewId} for product {params.productId}</h1>
+    )
 }
